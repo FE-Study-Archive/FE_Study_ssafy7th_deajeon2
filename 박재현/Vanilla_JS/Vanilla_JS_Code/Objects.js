22b46416774811ae59855a8c89499d5ec2bcb026
JHPark <erict4f@gmail.com>
2022-02-26 23:24:12 +0900

    JH_Update: Vanilla JS Chapter02 9th Clear

---
+++ 박재현/Vanilla_JS/Vanilla_JS_Code/Objects.js
@@ -0,0 +1,15 @@
+// const playerName = "Jaehyun";
+// const playerPoints = 121212;
+// const playerHandsome = false;
+// const playerFat = "chubby"
+
+const player = {
+  name : 'Jaehyun',
+  points : 10,
+  fat : true,
+};
+console.log(player);
+player.fat = false;
+console.log(player);
+player.lastName = "Park";
+console.log(player)

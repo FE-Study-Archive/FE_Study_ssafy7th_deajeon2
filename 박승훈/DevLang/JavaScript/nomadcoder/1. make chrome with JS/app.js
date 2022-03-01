e1fc8e158c66fd9610d61a725f3773824397287f
Orchemi <tmdgns1126@naver.com>
2022-03-02 02:12:18 +0900

    SH_Update: Vanilla JS Chapter 03 03

--- 박승훈/DevLang/JavaScript/nomadcoder/1. make chrome with JS/app.js
+++ 박승훈/DevLang/JavaScript/nomadcoder/1. make chrome with JS/app.js
@@ -1,6 +1,23 @@
-let a = 5;
-let b = 10;
+const title = document.querySelector(".hello h1");
 
-console.log(a+b);
-console.log(a*b);
-console.log(a/b);
+function handleTitleClick(){
+  if (title.style.color=="brown") {
+    title.style.color = "blue";
+  } else if (title.style.color=="blue") {
+    title.style.color = "red";
+  } else {
+    title.style.color = "brown";
+  }
+}
+
+function handlerTitleMouseOver(){
+  title.style.opacity = 0.5;
+}
+
+function handlerTitleMouseOut(){
+  title.style.opacity = 1;
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseover", handlerTitleMouseOver);
+title.addEventListener("mouseout", handlerTitleMouseOut);

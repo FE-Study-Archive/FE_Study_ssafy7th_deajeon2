c8effaace8cd6f7e5400d6c70e7c70d8a07d69ab
wonwoo <lww0407@naver.com>
2022-03-18 02:57:46 +0900

    WW_Add: Personal JS Project Accomplish

---
+++ 이원우/JS_project/js/bg.js
@@ -0,0 +1,8 @@
+const loginForm = document.querySelector('#login-form')
+
+
+backgroundImage.src = `img/${image_bg[1]}`;
+backgroundImage.classList.add("background");
+
+document.body.prepend(backgroundButton);
+
